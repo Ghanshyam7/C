cfa8a4ece241ed3a4c8132a0d61ebb115daa5957
Ghanshyam7 <74923482+Ghanshyam7@users.noreply.github.com>
2020-11-24 10:29:49 +0530

    finding sum by taking input from user

---
+++ inputfromuser=sum.c
@@ -0,0 +1,14 @@
+//Program to take input from user and do additions
+
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter first number - ");
+    scanf("%d",&a);
+    printf("enter second number - ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum = %d",sum);
+    return 0;
+}
