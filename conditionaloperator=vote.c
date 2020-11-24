fb21d05a1eae5a3e14f3fd718da119e6c931901c
Ghanshyam7 <74923482+Ghanshyam7@users.noreply.github.com>
2020-11-24 10:19:36 +0530

    voting using conditional operator

---
+++ conditionaloperator=vote.c
@@ -0,0 +1,10 @@
+//H.W Problem :
+#include<stdio.h>
+int main()
+{
+    int x=18,y;
+    printf("enter your age : ");
+    scanf("%d",&y);
+    (y>=x)?printf("eligible to cast vote"):printf("not eligible to cast vote");
+    return 0;
+}
