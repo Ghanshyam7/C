2fe056406e09db2824e7b49dc377ac1bbe7e8807
Ghanshyam7 <74923482+Ghanshyam7@users.noreply.github.com>
2020-11-24 10:29:32 +0530

    taking input from the user

---
+++ inputfromuser.c
@@ -0,0 +1,10 @@
+//Program to take input from user
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter a number - ");
+    scanf("%d",&number);
+    printf("your entered number is %d",number);
+    return 0;
+}
