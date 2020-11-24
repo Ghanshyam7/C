10dbe59a5d2a1cf113e717a313e628d7e4f7641e
Ghanshyam7 <74923482+Ghanshyam7@users.noreply.github.com>
2020-11-24 10:30:26 +0530

    basic math function usage

---
+++ mathfunction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    printf("%f\n",ceil(9.2));
+    printf("%f\n",floor(9.2));
+    printf("%f\n",pow(2,3));
+    printf("%f\n",sin(35));
+    printf("%f\n",cos(0));
+    printf("%f\n",log(2));
+    printf("%f\n",fabs(-3.001));
+    printf("%f\n",exp(3.2));
+}
